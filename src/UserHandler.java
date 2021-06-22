212090c9557546b8d0d7022b622be69edc0b39bd
sina <sinatb.dev@gmail.com>
2021-06-22 18:49:15 +0430

    added credit adding

--- src/UserHandler.java
+++ src/UserHandler.java
@@ -1,4 +1,10 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.Socket;
+import java.util.Map;
 
 public class UserHandler extends ClientHandler{
 
@@ -9,6 +15,35 @@ public class UserHandler extends ClientHandler{
 
     @Override
     public void run() {
+        try {
+            String Command = new String(dis.readNBytes(dis.readInt()));
+            String[] AnalyzableCommand = Command.split("-");
+            Gson gson = new Gson();
+            //userID = "U-phonenumber"
 
+            //order-userID-RestaurantID-OrderJson
+
+            //credit-userID-amount
+            if (AnalyzableCommand[0].equals("credit"))
+            {
+                String json = database.getJson(AnalyzableCommand[1]+"-"+AnalyzableCommand[2]);
+                Type type = new TypeToken<Map<String,String>>(){}.getType();
+                Map<String , String> jsonMap = gson.fromJson(json , type);
+                int currentCredit = Integer.parseInt(jsonMap.get("credit")) ;
+                currentCredit += Integer.parseInt(AnalyzableCommand[3]);
+                jsonMap.put("credit" , String.valueOf(currentCredit));
+
+                String newJson = gson.toJson(jsonMap);
+                dos.writeUTF(newJson);
+                database.saveChangeByID(AnalyzableCommand[1]+"-"+AnalyzableCommand[2] , newJson);
+
+            }
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
