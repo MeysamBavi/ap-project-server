66f8c5cfd8360b90b62baca0f91591900f8159d9
Meysam Bavi <meybsky@gmail.com>
2021-07-12 18:50:25 +0430

    Use CLI args for configuration

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,19 @@ import network.Server;
 import java.io.IOException;
 
 public class Main {
+
+    // args: [port, database directory, number of threads (optional)]
     public static void main(String[] args) throws IOException {
-        Database database = new Database("C:\\src\\database");
-        new Server(8081, database).run();
+
+        int port = Integer.parseInt(args[0]);
+        int threadCount = Server.DEFAULT_NUMBER_OF_THREADS;
+
+        try {
+            threadCount = Integer.parseInt(args[2]);
+        } catch (Exception ignored) {}
+
+        Database database = new Database(args[1]);
+
+        new Server(port, database, threadCount).run();
     }
 }
